140f24e7473cedace55850d29eef7eec2cd74004
Findupto <11663892+findupto@users.noreply.github.com>
2026-09-06 10:52:18 +0500

    Expand core database context

--- src/FindUpTo.Pos.Server/Data/CoreDbContext.cs
+++ src/FindUpTo.Pos.Server/Data/CoreDbContext.cs
@@ -5,6 +5,8 @@ namespace FindUpTo.Pos.Server.Data;
 
 public sealed class CoreDbContext(DbContextOptions<CoreDbContext> options) : DbContext(options)
 {
+    public DbSet<AppUser> Users => Set<AppUser>();
+    public DbSet<BusinessSetting> BusinessSettings => Set<BusinessSetting>();
     public DbSet<Category> Categories => Set<Category>();
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Customer> Customers => Set<Customer>();
@@ -13,6 +15,12 @@ public sealed class CoreDbContext(DbContextOptions<CoreDbContext> options) : DbC
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<AppUser>().HasIndex(x => x.Username).IsUnique();
+        modelBuilder.Entity<AppUser>().Property(x => x.Username).HasMaxLength(64);
+        modelBuilder.Entity<AppUser>().Property(x => x.Role).HasMaxLength(32);
+        modelBuilder.Entity<BusinessSetting>().Property(x => x.BusinessName).HasMaxLength(160);
+        modelBuilder.Entity<BusinessSetting>().Property(x => x.CurrencyCode).HasMaxLength(8);
+        modelBuilder.Entity<BusinessSetting>().Property(x => x.CurrencySymbol).HasMaxLength(16);
         modelBuilder.Entity<Category>().Property(x => x.Name).HasMaxLength(100);
         modelBuilder.Entity<Product>().Property(x => x.Name).HasMaxLength(160);
         modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2);
